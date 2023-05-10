7a10ac2456a605c5f7e87e75eec39a18e6659be2
MarcelJuca <marceljuca@edu.unifor.br>
2023-05-10 14:41:43 -0300

    treatString() to parse expression and remove the outtest expression

--- app/parser/tester.py
+++ app/parser/tester.py
@@ -1,23 +1,38 @@
+from treeGraph import TreeGraph
 from validator import SQLQueryValidator
 
 # Create SQL query validator instance
 validator = SQLQueryValidator()
 #parser = Parser()
 
-# Sample SQL queries to validate
-query1 = "Select id, name, email From users Where id = 1;"
-query2 = "SELECT name, price FROM productsThisTableDoesntExist WHERE price < 10.0 ORDER BY name;"
-query3 = "SELECT Nome, DataNascimento, Descricao, SaldoInicial FROM Usuario join Contas on Usuario.idUsuario = Contas.Usuario_idUsuario WHERE SaldoInicial >= 235 and UF ='ce' and CEP <> '62930000';"
-query4 = "SELECT idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição FROM usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta WHERE saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3; "
-query5 = "SELECT idUsuario FROM Usuario JOIN Contas ON Usuario.idUsuario = Contas.Usuario_idUsuario;"
-query6 = "SELECT idUsuario FROM Usuario;"
+# # Sample SQL queries to validate
+# query1 = "Select id, name, email From users Where id = 1;"
+# query2 = "SELECT name, price FROM productsThisTableDoesntExist WHERE price < 10.0 ORDER BY name;"
+# query3 = "SELECT Nome, DataNascimento, Descricao, SaldoInicial FROM Usuario join Contas on Usuario.idUsuario = Contas.Usuario_idUsuario WHERE SaldoInicial >= 235 and UF ='ce' and CEP <> '62930000';"
+# query4 = "SELECT idusuario, nome, datanascimento, descricao, saldoinicial, UF, Descrição FROM usuario join contas on usuario.idUsuario = contas.Usuario_idUsuario join tipoconta on tipoconta.idTipoConta = contas.TipoConta_idTipoConta WHERE saldoinicial < 3000 and uf = 'ce' and Descrição <> 'Conta Corrente' and idusuario > 3; "
+# query5 = "SELECT idUsuario FROM Usuario JOIN Contas ON Usuario.idUsuario = Contas.Usuario_idUsuario;"
+# query6 = "SELECT idUsuario FROM Usuario;"
+# # Validate SQL queries
+# print(validator.validate_query(query1))  # True
+# print(validator.validate_query(query2))  # False
+# print(validator.validate_query(query3))  # True
+# print(validator.validate_query(query4))  # True
+# print(validator.validate_query(query5))  # True
+# print(validator.validate_query(query6))  # True
 
-# Validate SQL queries
-print(validator.validate_query(query1))  # True
-print(validator.validate_query(query2))  # False
-print(validator.validate_query(query3))  # True
-print(validator.validate_query(query4))  # True
-print(validator.validate_query(query5))  # True
-print(validator.validate_query(query6))  # True
+inputString = "	( (A(B(C))) D (E(F))) G (H((I (J))))"
+output_path = './app/RelationalTree'
+#Create a TreeGraph object, generate and export the graph as 'RelationalTree.png'
+tree = TreeGraph(inputString, output_path)
 
+cases = [
+    "( (A(B(C))) D (E(F))) GEEE (H((I (J))))",
+    "GEEE (H((I (J))))",
+    "( (A(B(C))) D (E(F))) GEEE",
+    "GEEE",
+    "((GEE))",
+    ""
+    ]
+for case in cases:
+    print(tree.treatString(case))  
 
--- app/parser/treeGraph.py
+++ app/parser/treeGraph.py
@@ -1,3 +1,5 @@
+import re
+
 import graphviz
 from graphviz import Digraph
 
@@ -29,7 +31,79 @@ class TreeGraph:
             return
         else:
             #remove string outside brackets
+            trimmedExpression, remainingExpression = treatString(expression)
+            
             #if there is brackets in the entire expression, remove first and last brackets
             #call recursiveQueryTree(modifiedExpresion) and possibly branch in case of join clause
 
     #aux methods
+    def treatString(self, expression):
+        remainder = []
+        trimmedExpression = ""
+        stack = []
+        indexToBeginningOfTrimmed = -1
+        i = 0 
+        ignoredCharacters = ['\t', " "]
+
+        while i < len(expression):
+            character = expression[i]
+            if character == '(':
+                if trimmedExpression != "": #found other opening brackets but already have a trimmedExpression; split expression and return
+                    if (not expression[:indexToBeginningOfTrimmed].isspace()) and expression[:indexToBeginningOfTrimmed] != "":
+                        remainder.append(expression[:indexToBeginningOfTrimmed])
+                    remainder.append(expression[i:])
+                    break
+                else:
+                    stack.append(character)
+            elif character == ')':
+                    stack.pop()
+            else:
+                if len(stack) > 0:
+                    i = i + 1
+                    continue
+                else: #found expression outside brackets
+                    if character not in ignoredCharacters:
+                        if indexToBeginningOfTrimmed == -1:
+                            indexToBeginningOfTrimmed = i
+                        trimmedExpression += character                               
+            i = i + 1
+            
+        if expression != "" and trimmedExpression == "": 
+            return ( self.treatString(str(expression[1:-1])) )  #execute again with outter brackets trimmed
+        if trimmedExpression != "" and remainder == []: #trimmedExpression was at the very end of the input
+            remainder.append(expression[:indexToBeginningOfTrimmed])
+
+        return trimmedExpression, remainder
+    
+
+    # def GPTtreatString(expression):
+    #     if '(' not in expression and ')' not in expression:
+    #         return expression, []
+
+    #     stack = []
+    #     trimmedExpression = ""
+    #     remainingExpressions = []
+    #     i = 0
+    #     while i < len(expression):
+    #         if expression[i] == '(':
+    #             stack.append('(')
+    #             if trimmedExpression == "" and len(stack) == 1:
+    #                 trimmedExpression = expression[:i].strip()
+    #                 expression = expression[i:]
+    #                 i = -1
+    #         elif expression[i] == ')':
+    #             if stack and stack[-1] == '(':
+    #                 stack.pop()
+    #             if not stack and trimmedExpression == "":
+    #                 trimmedExpression = expression[:i+1].strip()
+    #                 expression = expression[i+1:]
+    #                 i = -1
+    #         i += 1
+
+    #     if trimmedExpression and trimmedExpression[0] == '(' and trimmedExpression[-1] == ')':
+    #         return treatString(trimmedExpression[1:-1])
+
+    #     if expression:
+    #         remainingExpressions.append(expression)
+
+    #     return trimmedExpression, remainingExpressions
